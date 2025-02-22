e4aee7f6fb4b3e79001d91734a41cdd491bacc4d
Yehor Dudnik <yehor.dudnik@gmail.com>
2025-02-22 18:07:58 +0000

    Migration and initial models

--- MaestroApp/models.py
+++ MaestroApp/models.py
@@ -1,41 +1,38 @@
 from django.db import models
 
-# Create your models here.
-# from django.contrib.auth.models import AbstractUser
-#
-#
-# class MaestroUser(AbstractUser):
-#     ROLE_CHOICES = (
-#         ('admin', 'Admin'),
-#         ('teacher', 'Teacher'),
-#         ('student', 'Student'),
-#     )
-#
-#     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
-#     classes = models.ManyToManyField('MaestroClass', related_name='users', blank=True)
-#
-#     def __str__(self):
-#         return self.first_name + self.last_name
-#
-#
-# class MaestroClass(models.Model):
-#     title = models.CharField(max_length=100)
-#
-#     teacher = models.ForeignKey(
-#         'MaestroUser',
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         blank=True,
-#         related_name='teaching_classes',
-#         limit_choices_to={'role': 'teacher'}
-#     )
-#
-#     students = models.ManyToManyField(
-#         'MaestroUser',
-#         related_name='enrolled_classes',
-#         blank=True,
-#         limit_choices_to={'role': 'student'}
-#     )
-#
-#     def __str__(self):
-#         return self.title
+from django.contrib.auth.models import AbstractUser, Permission, Group
+
+
+class MaestroRole(models.Model):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
+
+def get_default_role():
+    return MaestroRole.objects.get_or_create(role="student")[0].id
+
+
+class MaestroUser(AbstractUser):
+    role = models.OneToOneField(MaestroRole, on_delete=models.SET_DEFAULT, default=get_default_role,
+                                related_name='user')
+    groups = models.ManyToManyField(
+        Group,
+        related_name="maestro_users_groups",
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="maestro_users_permissions",
+        blank=True
+    )
+    password_hash = models.CharField(max_length=256)
+    balance = models.DecimalField(decimal_places=3, max_digits=7)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
