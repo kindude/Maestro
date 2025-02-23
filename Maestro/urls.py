aee9f1f7aa9b148b7f8d48c09a6730d23f0a4b63
Yasaman <3020308m@student.gla.ac.uk>
2025-02-23 20:58:11 +0000

    Added accounts app

--- Maestro/urls.py
+++ Maestro/urls.py
@@ -21,5 +21,6 @@ from MaestroApp.urls import urlpatterns as app_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(app_patterns))
-]
+    path('', include(app_patterns)),
+    path('accounts/', include('accounts.urls')),
+    ]
