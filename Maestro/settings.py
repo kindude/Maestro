b35e3a6056a88d5b4d3f50d89edae8ef283b5fdf
Yehor Dudnik <yehor.dudnik@gmail.com>
2025-02-25 00:13:22 +0000

    Static url folder

--- Maestro/settings.py
+++ Maestro/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'MaestroApp/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MaestroApp/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
