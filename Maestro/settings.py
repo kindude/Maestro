d167c755eb640e64d8788fa6514d30c6d2af6625
Yehor Dudnik <yehor.dudnik@gmail.com>
2025-02-26 20:34:13 +0000

    Fixing my favourite interactive piano

--- Maestro/settings.py
+++ Maestro/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'MaestroApp/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MaestroApp/static')]
 
 # Default primary key field type
